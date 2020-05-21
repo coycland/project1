4c301ec6172d596e1cc02b0d09227e00f21e9ac8
Artem <coycland@gmail.com>
2020-05-21 17:31:29 +0300

    practice2

--- js/practice1_project.js
+++ js/practice1_project.js
@@ -12,13 +12,30 @@ count: numberOffFilms,
   privat: false
 };
 
-const lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
-      reviewFilm = prompt("На сколько оцените его?", ""),
-      lastFilm2 = prompt("Один из последних просмотренных фильмов?", ""),
-      reviewFilm2 = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[lastFilm] = reviewFilm;    // если бы lastFilm записали через  . то кириличесские данные записались бы неверно
-personalMovieDB.movies[lastFilm2] = reviewFilm2;
 
-console.log(numberOffFilms);
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;   // если бы lastFilm записали через  . то кириличесские данные записались бы неверно
+      console.log('Done');
+  } else {
+    console.log('Error');
+    i--;
+  }
+       
+}
+
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
+
 console.log(personalMovieDB);
